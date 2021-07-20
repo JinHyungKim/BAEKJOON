37cf630e0472d6a146a1f4f9e231d280fb4c0398
JinHyungKim <83858427+JinHyungKim@users.noreply.github.com>
2021-07-20 23:27:07 +0900

    Create 1152.java
    단어의 개수

---
+++ 1152.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		System.out.println(st.countTokens());
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
