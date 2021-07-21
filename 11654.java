450cbb22505b2dca7ee97197862d582261279eca
JinHyungKim <83858427+JinHyungKim@users.noreply.github.com>
2021-07-21 22:16:07 +0900

    Create 11654.java
    아스키 코드

---
+++ 11654.java
@@ -0,0 +1,16 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		char a = sc.next().charAt(0);
+		System.out.println((int)a);
+	}
+}
